962a7f937d59cf0ba95cac143ee6ae34283d6781
vasil9evdenis <vasil9evdenis@icloud.com>
2020-06-11 14:53:46 +0300

    Закончил вёрстку проекта

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openPopup () {
+	var button = document.querySelector('.button');
+	button.onclick = function () {
+		var popup = document.querySelector('.search-form');
+		popup.classList.toggle('close');
+	}
+}
+
+openPopup ();
+
+
