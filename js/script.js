60de766a939f6c16dd36a12800bb507ca796cdb0
vasil9evdenis <vasil9evdenis@icloud.com>
2020-06-12 00:51:07 +0300

    Сделал анимацию

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function openPopup () {
 	var button = document.querySelector('.button');
 	button.onclick = function () {
 		var popup = document.querySelector('.search-form');
-		popup.classList.toggle('close');
+		popup.classList.toggle('show');
 	}
 }
 
